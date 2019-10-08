accaac81a3709fa0fa610df4052904a91971f239
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-08 16:13:14 -0500

    added index server and first migration

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+
+const accountsR = require('./data/accountsRouter.js');
+server.use('/api/accounts', accountsR);
+
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda WEP DB Challenge :)</h>
+      <p>Let's get started... </p>
+    `);
+});
+
+module.exports = server;
